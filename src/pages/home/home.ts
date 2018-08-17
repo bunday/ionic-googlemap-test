7563015746d47cf894d7c6b60c391c0dba5c5c80
Zadat Olayinka <zadat01@shoppi.ng>
2018-08-17 08:54:07 +0100

    Map basic test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  Marker,
+  LocationService
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  map:GoogleMap;
+  latitude: any = 8.561542;
+  longitude: any = 7.351373;
   constructor(public navCtrl: NavController) {
+    setTimeout(this.loadMap.bind(this),2000);
+  }
+  loadMap(){
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: this.latitude,
+          lng: this.longitude
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
 
+    this.map = GoogleMaps.create('mapperFacade', mapOptions)
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+        this.map.addMarker({
+          title: 'Prospect Location',
+          icon: 'blue',
+          animation: 'DROP',
+          position: {
+            lat: this.latitude,
+            lng: this.longitude
+          }
+        })
+          .then(marker => {
+            marker.on(GoogleMapsEvent.MARKER_CLICK)
+              .subscribe(() => {
+
+              });
+          });
+
+      });
   }
 
 }
